bef0abca921ca12c059511731de3cd69619f55e3
artemetra <48987557+artemetra@users.noreply.github.com>
2021-01-02 00:23:47 +0200

    Update cum.py

--- cum.py
+++ cum.py
@@ -2,14 +2,11 @@
 
 from telethon import events
 import asyncio
-from collections import deque
 
-@borg.on(events.NewMessage(pattern=r"\.rain", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.bruh", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    deq = deque(list("☁️☁️🌧🌩⛈🌨🌪🌪🌊"))
     for _ in range(100):
         await asyncio.sleep(0.1)
-        await event.edit("".join(deq))
-        deq.rotate(1)
+        await event.edit("".join("cum"))
