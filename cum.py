95cd370e97201664c90bc09b6467afa9a73dfa04
artemetra <48987557+artemetra@users.noreply.github.com>
2021-01-02 01:27:40 +0200

    Update cum.py

--- cum.py
+++ cum.py
@@ -3,11 +3,11 @@
 from telethon import events
 import asyncio
 
+i = 10
+
 @borg.on(events.NewMessage(pattern=r"\.bruh", outgoing=True))
 async def _(event):
     strings = {'name': 'bruh'}
     if event.fwd_from:
         return
-    for i in range(100):
-        await asyncio.sleep(0.1)
-        await event.edit("cum " + str(i))
+    await event.edit("cum " + str(i))
