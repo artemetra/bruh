3db2569d1bde60cc6ac37e7e5f59419e5b7802d4
artemetra <48987557+artemetra@users.noreply.github.com>
2021-01-26 00:06:19 +0200

    Update unshorten.py

--- unshorten.py
+++ unshorten.py
@@ -6,10 +6,15 @@ import random
 from userbot.events import register
 import requests
 
-@register(outgoing=True, pattern="^.unsh (.*)")
+def register(cb):
+	cb(unsh())
 
-async def brhu(self, message):
-    '''Расшифровывание укорочённых ссылок (bit.ly, goog.le и т.д.)'''
-    test = requests.get('https://unshorten.me/s/goo.gl/IGL1lE')
-    test.json()
-    
+class unsh(loader.Module):
+    strings = {'name': 'Unshorten Links'}
+    async def unsh(self, message):
+        
+        '''Расшифровывание укорочённых ссылок (bit.ly, goog.le и т.д.)'''
+        test = requests.get('https://unshorten.me/s/goo.gl/IGL1lE')
+        test.json()
+        reply = await event.get_reply_message()
+        await event.client.send_message(event.to_id, test, reply_to=reply)
