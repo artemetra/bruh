9646b1e165035521b7aac44f568b335c26842d1d
artemetra <48987557+artemetra@users.noreply.github.com>
2021-01-26 23:35:31 +0200

    Update unshorten.py

--- unshorten.py
+++ unshorten.py
@@ -3,14 +3,17 @@ from random import choice, randint
 from telethon import functions
 from .. import loader, utils
 import random
-from userbot.events import register
+from userbot.messages import register
 import requests
 
+#def register(cb):
+#	cb(unsh())
 
 class unsh(loader.Module):
     strings = {'name': 'Unshorten Links'}
-    async def unsh(self, message, event):
+    async def unsh(self, message):
+        
         test = requests.get('https://unshorten.me/s/goo.gl/IGL1lE')
         #test.json()
-        reply = await event.get_reply_message()
-        await event.client.send_message(event.to_id, test, reply_to=reply)
+        reply = await message.get_reply_message()
+        await message.client.send_message(message.to_id, test, reply_to=reply)
