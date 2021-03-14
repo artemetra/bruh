57e6e852fbe67c66173662d131cd29d0a98ce93b
artemetra <48987557+artemetra@users.noreply.github.com>
2021-03-14 19:41:53 +0200

    Update cum.py

--- cum.py
+++ cum.py
@@ -1,15 +1,26 @@
-#tset
-
-from telethon import events
 import asyncio
-import requests
+import logging
 
-@borg.on(events.NewMessage(pattern=r"\.bruh", outgoing=True))
-async def _(event):
-    strings = {'name': 'bruh'}
-    #if event.fwd_from:
-    #    return
-    test = requests.get('https://unshorten.me/s/goo.gl/IGL1lE')
-    
-    await event.edit(str(test.text))
-    
+from .. import loader, utils
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class YourMod(loader.Module):
+    """unshorten shortened links"""  # Translateable due to @loader.tds
+    strings = {"cfg_doc": "This is what is said, you can edit me with the configurator",
+               "name": "unsh",
+               "after_sleep": "We have finished sleeping!"}
+
+    def __init__(self):
+        self.config = loader.ModuleConfig("CONFIG_STRING", "hello", lambda m: self.strings("cfg_doc", m))
+
+    @loader.unrestricted  # Security setting to change who can use the command (defaults to owner | sudo)
+    async def examplecmd(self, message):
+        """Does something when you type .example (hence, named examplecmd)"""
+        logger.debug("We logged something!")
+        await utils.answer(message, self.config["CONFIG_STRING"])
+        await asyncio.sleep(5)  # Never use time.sleep
+        await utils.answer(message, self.strings("after_sleep", message))
+
