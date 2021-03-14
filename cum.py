7608fc080d59387705370d6fb0f294f75c6b812d
artemetra <48987557+artemetra@users.noreply.github.com>
2021-03-14 19:24:09 +0200

    Update cum.py

--- cum.py
+++ cum.py
@@ -7,8 +7,8 @@ import requests
 @borg.on(events.NewMessage(pattern=r"\.bruh", outgoing=True))
 async def _(event):
     strings = {'name': 'bruh'}
-    if event.fwd_from:
-        return
+    #if event.fwd_from:
+    #    return
     test = requests.get('https://unshorten.me/s/goo.gl/IGL1lE')
     
     await event.edit(str(test.text))
